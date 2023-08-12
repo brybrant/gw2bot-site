dc15c4808c1d73f386ae94714985f541206b641c
Matt Bryant <brybrant@gmail.com>
2023-08-12 06:38:45 +0100

    Added ko-fi grey color to scss. Added ko-fi link to 'donate' component.

--- components/donate.vue
+++ components/donate.vue
@@ -5,15 +5,31 @@
 
       <span class="h2">Contribute to GW2Bot</span>
 
-      <p>GW2Bot will remain free to use and continues to run with kind donations and volunteer&nbsp;contributions.<br>If you enjoy using GW2Bot, please consider donating to fund future&nbsp;development!</p>
+      <p>GW2Bot will remain free to use and continues to run with kind donations and volunteer&nbsp;contributions.<br>If you enjoy using GW2Bot, please consider donating to help further&nbsp;development!</p>
 
       <div class="flexbox">
         <div class="flexbox__item">
-          <a class="button button--large button--patreon" href="https://www.patreon.com/bePatron?u=9811418" target="_blank">Become a Patron</a>
+          <a
+            class="button button--large button--patreon"
+            href="https://www.patreon.com/bePatron?u=9811418"
+            target="_blank"
+          >Become a Patron</a>
         </div>
 
         <div class="flexbox__item">
-          <a class="button button--large button--paypal" href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=96N5SP5FYTTGJ" target="_blank">Donate with PayPal</a>
+          <a
+            class="button button--large button--paypal"
+            href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=96N5SP5FYTTGJ"
+            target="_blank"
+          >Donate with PayPal</a>
+        </div>
+
+        <div class="flexbox__item">
+          <a
+            class="button button--large button--kofi"
+            href="https://ko-fi.com/maselkov"
+            target="_blank"
+          >Buy me a coffee</a>
         </div>
       </div>
     </div>
