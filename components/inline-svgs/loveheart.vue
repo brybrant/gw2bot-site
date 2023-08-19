cb95449a34fa04b71ce738605c9be254f26688c3
Matt Bryant <brybrant@gmail.com>
2023-08-19 09:43:09 +0100

    Remove redundant class from loveheart inline-svg

--- components/inline-svgs/loveheart.vue
+++ components/inline-svgs/loveheart.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg class="inline-svg loveheart-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 600">
+  <svg class="inline-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 600">
     <path d="M389,144c-43,0-69.5,31.77-89,75-19.5-43.23-46-75-89-75-54,0-82,41-82,95,0,67,38.5,114,171,229,132.5-115,171-162,171-229,0-54-28-95-82-95Z" />
   </svg>
 </template>
