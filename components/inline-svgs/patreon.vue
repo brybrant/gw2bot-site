a3008100da8c94b9306dcdbf5ffe7e6fc59ef48c
Matt Bryant <brybrant@gmail.com>
2023-08-19 09:44:33 +0100

    Added patreon inline-svg

---
+++ components/inline-svgs/patreon.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 600">
+    <circle cx="366.87" cy="249.94" r="149.94" />
+    <rect x="100" y="100" width="73.25" height="400" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'PatreonInlineSVG'
+}
+</script>
+
+<style scoped>
+circle, rect {
+  fill: currentColor;
+}
+</style>
